1fd38da421f361b04710b4df9e80d839185fb787
Boris Rumyantsev <bdrum@jinr.ru>
2020-03-29 07:06:13 +0300

    Timeout task

--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Diagnostics;
 using System.Threading.Tasks;
 using System.Threading;
 
@@ -7,61 +6,46 @@ namespace flow
 {
     class Program
     {
-        static void Method1()
+        static async Task DoSomeThingAsync()
         {
-            for (var i = 0; i < 2; ++i)
-            {
-                Thread.Sleep(1000);
-                Console.WriteLine("Method1 is working...");
-            }
+            Console.WriteLine("DoSomeThingAsync:This was call synchroniously");
+
+            //await Task.Delay(2000).ConfigureAwait(false);
+            await Task.Delay(5000);
+
+            Console.WriteLine("DoSomeThingAsyncThis was call after async pause");
         }
 
-        static void Method2()
+        static void DoSomeThing(string a)
         {
-            for (var i = 0; i < 4; ++i)
-            {
-                Thread.Sleep(1000);
-                Console.WriteLine("Method2 is working...");
-            }
-        }
-
-
-        static void Method3()
-        {
-            Console.WriteLine("Start reading file async...");
-            using (var file = new System.IO.StreamReader(@"D:\GoogleDrive\Study\notes\Programming\csharp\examples\flow\appsettings.json"))
-                while (!file.EndOfStream)
-                {
-                    Console.WriteLine(file.ReadLine());
-                    Thread.Sleep(1000);
-                }
-        }
-
-        static void SlowMethod()
-        {
-            Method1();
-            Method2();
-            Method3();
-
-            Console.WriteLine($"Slow method completed");
-
+            Console.WriteLine(a);
+            System.Threading.Thread.Sleep(5000);
         }
 
         static void Main(string[] args)
         {
-            var st = Stopwatch.StartNew();
-            Console.WriteLine($"Start Main sync.:");
-            SlowMethod();
+            CallMe();
 
-            for (var i = 0; i < 5; ++i)
-            {
-                Thread.Sleep(1000);
-                Console.WriteLine($"CallMethod is working...");
-            }
+            Console.WriteLine("This called after 'CallMe'");
+            Console.ReadLine();
+        }
+        static async Task CallMe()
+        {
+            Console.WriteLine("CallMe:Start app:");
+            var cts = new CancellationTokenSource(TimeSpan.FromSeconds(3));
+            var timeout = Task.Delay(Timeout.InfiniteTimeSpan, cts.Token);
 
-            st.Stop();
-            Console.WriteLine($"Main method has finished working");
-            Console.WriteLine($"Elapsed time {st.ElapsedMilliseconds / 1000} sec");
+            var task = new Task<Task>(DoSomeThingAsync, cts.Token);
+            var task1 = new Task(() => DoSomeThing("this is arg"), cts.Token);
+            var t = await Task.WhenAny(timeout, task);
+
+            if (t == timeout)
+                Console.WriteLine("TimeOut!");
+
+            //await DoSomeThingAsync();
+
+            Console.WriteLine("End app");
+
         }
     }
 }
