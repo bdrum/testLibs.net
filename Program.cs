035fd5f80219f178d138ed865fcdeaabb7eb91fc
Boris Rumyantsev <bdrum@jinr.ru>
2020-04-03 10:50:11 +0300

    Parallel.For matrix multiplication

--- Program.cs
+++ Program.cs
@@ -1,45 +1,113 @@
 ﻿using System;
-using System.Threading;
+using System.Diagnostics;
 using System.Threading.Tasks;
 
 namespace flow
 {
     class Program
     {
-        static async void Method1(CancellationToken ct)
+        #region Sequential_Loop
+        static void MultiplyMatricesSequential(double[,] matA, double[,] matB,
+                                                double[,] result)
         {
-            try
-            {
-                Console.WriteLine("Parallel task is begining:");
+            int matACols = matA.GetLength(1);
+            int matBCols = matB.GetLength(1);
+            int matARows = matA.GetLength(0);
 
-                for (var i = 0; i < 10; ++i)
+            for (int i = 0; i < matARows; i++)
+            {
+                for (int j = 0; j < matBCols; j++)
                 {
-                    await Task.Delay(TimeSpan.FromSeconds(1), ct);
-                    Console.WriteLine($"{i}_task");
+                    double temp = 0;
+                    for (int k = 0; k < matACols; k++)
+                    {
+                        temp += matA[i, k] * matB[k, j];
+                    }
+                    result[i, j] += temp;
                 }
             }
-            catch (OperationCanceledException oce)
-            {
-                Console.WriteLine(oce.Message);
-            }
         }
+        #endregion
 
-        static async Task Main(string[] args)
+        #region Parallel_Loop
+        static void MultiplyMatricesParallel(double[,] matA, double[,] matB, double[,] result)
         {
-            var canc = new CancellationTokenSource();
-            Console.WriteLine("Let's start in parallel:");
+            int matACols = matA.GetLength(1);
+            int matBCols = matB.GetLength(1);
+            int matARows = matA.GetLength(0);
 
-            await Task.Run(() => Method1(canc.Token), canc.Token);
-
-
-            for (var i = 0; i < 10; ++i)
+            // A basic matrix multiplication.
+            // Parallelize the outer loop to partition the source array by rows.
+            Parallel.For(0, matARows, i =>
             {
-                await Task.Delay(TimeSpan.FromSeconds(1));
-                Console.WriteLine($"{i}_main");
-                if (i == 4)
-                    canc.Cancel();
-            }
-            Console.ReadLine();
+                for (int j = 0; j < matBCols; j++)
+                {
+                    double temp = 0;
+                    for (int k = 0; k < matACols; k++)
+                    {
+                        temp += matA[i, k] * matB[k, j];
+                    }
+                    result[i, j] = temp;
+                }
+            }); // Parallel.For
         }
+        #endregion
+
+        #region Main
+        static void Main(string[] args)
+        {
+            // Set up matrices. Use small values to better view 
+            // result matrix. Increase the counts to see greater 
+            // speedup in the parallel loop vs. the sequential loop.
+            int colCount = 180;
+            int rowCount = 100;
+            int colCount2 = 170;
+            double[,] m1 = InitializeMatrix(rowCount, colCount);
+            double[,] m2 = InitializeMatrix(colCount, colCount2);
+            double[,] result = new double[rowCount, colCount2];
+
+            // First do the sequential version.
+            Console.Error.WriteLine("Executing sequential loop...");
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            MultiplyMatricesSequential(m1, m2, result);
+            stopwatch.Stop();
+            Console.Error.WriteLine("Sequential loop time in milliseconds: {0}",
+                                    stopwatch.ElapsedMilliseconds);
+
+            // Reset timer and results matrix. 
+            stopwatch.Reset();
+            result = new double[rowCount, colCount2];
+
+            // Do the parallel loop.
+            Console.Error.WriteLine("Executing parallel loop...");
+            stopwatch.Start();
+            MultiplyMatricesParallel(m1, m2, result);
+            stopwatch.Stop();
+            Console.Error.WriteLine("Parallel loop time in milliseconds: {0}",
+                                    stopwatch.ElapsedMilliseconds);
+
+            Console.Error.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+        #endregion
+
+        #region Helper_Methods
+        static double[,] InitializeMatrix(int rows, int cols)
+        {
+            double[,] matrix = new double[rows, cols];
+
+            Random r = new Random();
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    matrix[i, j] = r.Next(100);
+                }
+            }
+            return matrix;
+        }
+        #endregion
     }
 }
