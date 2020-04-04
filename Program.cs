e1c754a45bbfdb9630cc24e6810a1a9042474176
Boris Rumyantsev <bdrum@jinr.ru>
2020-04-04 14:56:33 +0300

    Add dotnetbenchmar

--- Program.cs
+++ Program.cs
@@ -1,18 +1,59 @@
 ﻿using System;
-using System.Diagnostics;
 using System.Threading.Tasks;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+// TODO: show how to break the Parallel.For
+// TODO: show how to get the state of Parallel.For
+// TODO: show how to add local var and sync(agregate) iteration between itself
 
 namespace flow
 {
-    class Program
+    [RPlotExporter, RankColumn]
+    public class MatrixMult
     {
+        private double[,] matA;
+        private double[,] matB;
+
+        #region Helper_Methods
+        public double[,] InitializeMatrix(int rows, int cols)
+        {
+            double[,] matrix = new double[rows, cols];
+
+            Random r = new Random();
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    matrix[i, j] = r.Next(100);
+                }
+            }
+            return matrix;
+        }
+        #endregion
+
+        [Params(200, 500, 1000, 2000)]
+        public int colCount;
+        [Params(200, 500)]
+        public int rowCount;
+        [Params(200)]
+        public int colCount2;
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            matA = InitializeMatrix(rowCount, colCount);
+            matB = InitializeMatrix(colCount, colCount2);
+        }
+
         #region Sequential_Loop
-        static void MultiplyMatricesSequential(double[,] matA, double[,] matB,
-                                                double[,] result)
+        [Benchmark]
+        public void MultiplyMatricesSequential()
         {
             int matACols = matA.GetLength(1);
             int matBCols = matB.GetLength(1);
             int matARows = matA.GetLength(0);
+            var result = new double[matARows, matBCols];
 
             for (int i = 0; i < matARows; i++)
             {
@@ -30,11 +71,13 @@ namespace flow
         #endregion
 
         #region Parallel_Loop
-        static void MultiplyMatricesParallel(double[,] matA, double[,] matB, double[,] result)
+        [Benchmark]
+        public void MultiplyMatricesParallel()
         {
             int matACols = matA.GetLength(1);
             int matBCols = matB.GetLength(1);
             int matARows = matA.GetLength(0);
+            var result = new double[matARows, matBCols];
 
             // A basic matrix multiplication.
             // Parallelize the outer loop to partition the source array by rows.
@@ -53,61 +96,13 @@ namespace flow
         }
         #endregion
 
-        #region Main
+    }
+
+    class Program
+    { 
         static void Main(string[] args)
         {
-            // Set up matrices. Use small values to better view 
-            // result matrix. Increase the counts to see greater 
-            // speedup in the parallel loop vs. the sequential loop.
-            int colCount = 180;
-            int rowCount = 100;
-            int colCount2 = 170;
-            double[,] m1 = InitializeMatrix(rowCount, colCount);
-            double[,] m2 = InitializeMatrix(colCount, colCount2);
-            double[,] result = new double[rowCount, colCount2];
-
-            // First do the sequential version.
-            Console.Error.WriteLine("Executing sequential loop...");
-            Stopwatch stopwatch = new Stopwatch();
-            stopwatch.Start();
-
-            MultiplyMatricesSequential(m1, m2, result);
-            stopwatch.Stop();
-            Console.Error.WriteLine("Sequential loop time in milliseconds: {0}",
-                                    stopwatch.ElapsedMilliseconds);
-
-            // Reset timer and results matrix. 
-            stopwatch.Reset();
-            result = new double[rowCount, colCount2];
-
-            // Do the parallel loop.
-            Console.Error.WriteLine("Executing parallel loop...");
-            stopwatch.Start();
-            MultiplyMatricesParallel(m1, m2, result);
-            stopwatch.Stop();
-            Console.Error.WriteLine("Parallel loop time in milliseconds: {0}",
-                                    stopwatch.ElapsedMilliseconds);
-
-            Console.Error.WriteLine("Press any key to exit.");
-            Console.ReadKey();
+            var b = BenchmarkRunner.Run<MatrixMult>();
         }
-        #endregion
-
-        #region Helper_Methods
-        static double[,] InitializeMatrix(int rows, int cols)
-        {
-            double[,] matrix = new double[rows, cols];
-
-            Random r = new Random();
-            for (int i = 0; i < rows; i++)
-            {
-                for (int j = 0; j < cols; j++)
-                {
-                    matrix[i, j] = r.Next(100);
-                }
-            }
-            return matrix;
-        }
-        #endregion
     }
 }
