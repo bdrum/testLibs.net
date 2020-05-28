501b14256f029d7e081b99eb2d78c7f3dd4b171c
Boris Rumyantsev <bdrum@jinr.ru>
2020-05-28 09:22:34 +0300

    WebDavApi test

--- Program.cs
+++ Program.cs
@@ -1,67 +1,22 @@
 ﻿using System;
+using System.IO;
 using System.Diagnostics;
 using System.Threading.Tasks;
 using System.Threading;
+using System.Net;
+using CsvHelper;
 
 namespace flow
 {
     class Program
     {
-        static void Method1()
+        static async Task Main(string[] args)
         {
-            for (var i = 0; i < 2; ++i)
-            {
-                Thread.Sleep(1000);
-                Console.WriteLine("Method1 is working...");
-            }
-        }
+            var _httpClient = new WebClient();
+            _httpClient.Credentials = new NetworkCredential("", "");
 
-        static void Method2()
-        {
-            for (var i = 0; i < 4; ++i)
-            {
-                Thread.Sleep(1000);
-                Console.WriteLine("Method2 is working...");
-            }
-        }
+            _httpClient.UploadData(@"https://disk.jinr.ru/remote.php/dav/files/regata/1107798.cnf", "PUT", File.ReadAllBytes(@"D:\Spectra\2020\01\dji-1\1107798.cnf"));
 
-
-        static void Method3()
-        {
-            Console.WriteLine("Start reading file async...");
-            using (var file = new System.IO.StreamReader(@"D:\GoogleDrive\Study\notes\Programming\csharp\examples\flow\appsettings.json"))
-                while (!file.EndOfStream)
-                {
-                    Console.WriteLine(file.ReadLine());
-                    Thread.Sleep(1000);
-                }
-        }
-
-        static void SlowMethod()
-        {
-            Method1();
-            Method2();
-            Method3();
-
-            Console.WriteLine($"Slow method completed");
-
-        }
-
-        static void Main(string[] args)
-        {
-            var st = Stopwatch.StartNew();
-            Console.WriteLine($"Start Main sync.:");
-            SlowMethod();
-
-            for (var i = 0; i < 5; ++i)
-            {
-                Thread.Sleep(1000);
-                Console.WriteLine($"CallMethod is working...");
-            }
-
-            st.Stop();
-            Console.WriteLine($"Main method has finished working");
-            Console.WriteLine($"Elapsed time {st.ElapsedMilliseconds / 1000} sec");
         }
     }
 }
