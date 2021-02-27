0c1836ded7a52978eb4898427ed71de07bc606cf
Boris Rumyantsev <bdrum@jinr.ru>
2021-02-27 14:09:32 +0300

    Circular list prototype

--- Program.cs
+++ Program.cs
@@ -1,11 +1,8 @@
 ﻿using System;
-using System.IO.MemoryMappedFiles;
-using System.IO;
-using System.Linq;
 using System.Diagnostics;
+using System.Threading;
 using BenchmarkDotNet.Running;
-using System.Text;
-using System.Collections.Generic;
+using System.Linq;
 namespace flow
 {
   public class Program
@@ -17,32 +14,14 @@ namespace flow
       var w = new Stopwatch();
       w.Start();
 
+      var cl = new CircleList<int>(Enumerable.Range(0, 3));
 
-      long l = 0;
-      using (var fs = new FileStream(LargeFileParser.tf, FileMode.Open, FileAccess.Read))
-        l = fs.Length;
-
-      Console.WriteLine(l);
-      Console.WriteLine((int)l);
-      Console.WriteLine(int.MaxValue / (1024 * 1024 * 1024));
-
-      var bts = new List<byte>(int.MaxValue - 1);
-
-      using (var m = MemoryMappedFile.CreateFromFile(LargeFileParser.tf, FileMode.Open))
+      foreach (var i in cl)
       {
-        using (var v = m.CreateViewAccessor(0, l))
-        {
-          var s = 100000;
-          for (long i = 0; i < l - 100000; i += s)
-          {
-            var b = new byte[s];
-            v.ReadArray(i, b, 0, b.Length);
-            bts.AddRange(b);
-          }
-        }
+        Console.WriteLine(i);
+        Thread.Sleep(1000);
       }
 
-      Console.WriteLine(Encoding.UTF8.GetString(bts.ToArray()).AsParallel().TakeLast(100).ToArray());
       Console.WriteLine($"Elapsed time: {w.Elapsed.TotalSeconds}");
       // var summary = BenchmarkRunner.Run<Sum>();
 
