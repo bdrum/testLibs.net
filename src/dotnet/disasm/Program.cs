225ec29517ee15d7483d9951282c7b6e57126d94
Boris Rumyantsev <bd.rumyantsev@gmail.com>
2021-10-06 17:19:23 +0300

    Wanted to remove sensitive data, but remove all...

---
+++ src/dotnet/disasm/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace disasm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ src/dotnet/leaks/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace leaks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
